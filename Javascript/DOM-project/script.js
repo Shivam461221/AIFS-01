4a9fe473a160468c67829ff363e0d885e41d60dd
Shivam Lowanshi <shivamlowanshi@gmail.com>
2025-03-29 20:41:51 +0530

    DOM project day-2

--- Javascript/DOM-project/script.js
+++ Javascript/DOM-project/script.js
@@ -8,9 +8,44 @@ function signUp(e){
         return;
     }
 
-    let user = {username, password};
+    let users = JSON.parse(localStorage.getItem('users')) || [];    
 
-    localStorage.setItem('user', JSON.stringify(user));
+    let existingUser = users.find(user=>user.username===username);  //null
+
+    if(existingUser){
+        alert('User already exist');
+        return;
+    }
+
+    let newUser = {username, password};
+
+    users.push(newUser);
+
+    localStorage.setItem('users', JSON.stringify(users));
     alert('Sign Up successful')
     window.location.href = 'Login.html';
+}
+
+function login(e){
+  e.preventDefault(); 
+  let username = document.getElementById('username').value;
+  let password = document.getElementById('password').value;
+
+  if(username==='' || password===''){
+      alert('All fields required');
+      return;
+  }
+
+  let users = JSON.parse(localStorage.getItem('users')) || [];
+
+  let foundUser = users.find(user=>user.username===username && user.password===password);
+
+  if(foundUser){
+    localStorage.setItem('loggedInUser', JSON.stringify(foundUser));
+    alert('Login Successful');
+    window.location.href = 'Dashboard.html'
+  }
+  else{
+    alert('Invalid Username and Password');
+  }
 }
