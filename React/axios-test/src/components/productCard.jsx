f341c9e31a30232c7f03e41699769ad3ab43e208
Shivam Lowanshi <shivamlowanshi@gmail.com>
2025-05-05 21:04:54 +0530

    React project, Template integration

--- React/axios-test/src/components/productCard.jsx
+++ React/axios-test/src/components/productCard.jsx
@@ -1,8 +1,21 @@
+
+
 export default function ProductCard({productList}){
-    
 
     return (
         <>
+        <div>
+            <div className="row">
+           { productList.map((product, index) => <div key={index} className="col-md-3 p-3">
+                    <img src={product.thumbnail} style={{width:"100%", height:"400px"}} />
+                    <h1>{product.title}</h1>
+                    <h3>RS. {product.price}</h3>
+                    <p>{product.description}</p>
+                    <button className="btn btn-primary form-control">View More</button>
+                    </div>)    
+          }
+          </div>
+        </div>
         </>
     )
 }
