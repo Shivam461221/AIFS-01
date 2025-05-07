cc02a911e1c025c195cdbae640d7d97f0b1a883e
Shivam Lowanshi <shivamlowanshi@gmail.com>
2025-05-07 21:12:31 +0530

    React project routing and API calling

--- React/clinic-application/src/App.jsx
+++ React/clinic-application/src/App.jsx
@@ -3,11 +3,38 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './components/Home'
+import Header from './components/Header'
+import Book from './components/Book'
+import About from './components/About'
+import Treatment from './components/Treatment'
+import Testimonial from './components/Testimonial'
+import Footer from './components/Footer'
+import Doctors from './components/Doctors'
+import Contact from './components/Contact'
+import { Route, Routes } from 'react-router-dom'
+import Login from './components/Login'
+import Dashboard from './components/Dashboard'
 
 function App() {
   return (
     <>
-      <Home/>
+      <Header />
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/book"} element={<Book />} />
+        <Route path={"/about"} element={<About />} />
+        <Route path={"/treatment"} element={<Treatment />} />
+        <Route path={"/doctors"} element={<Doctors />} />
+        <Route path={"/testimonial"} element={<Testimonial />} />
+        <Route path={"/contact"} element={<Contact />} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/dashboard' element={<Dashboard/>} />
+      </Routes>
+      <Footer />
+
+
+
+
     </>
   )
 }
---
+++ React/clinic-application/src/components/Login.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react"
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const login = async (e) => {
+        e.preventDefault();
+        try {
+            let response = await axios.post('http://localhost:8000/api/auth/login', { email, password });
+            if (response) {
+                navigate('/dashboard');
+                console.log(response.data);
+            }
+            else {
+                navigate('/');
+            }
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    console.log(email);
+    return (
+        <>
+            <section className="book_section layout_padding">
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <form>
+                                <h4>
+                                    LOG<span>IN</span>
+                                </h4>
+                                <div className="form-row ">
+                                    <div className="form-group col-lg-5">
+                                        <label for="inputPatientName">Email</label>
+                                        <input type="text" onChange={(event) => setEmail(event.target.value)} className="form-control" id="inputPatientName" placeholder="yourmail@gmail.com" />
+                                    </div>
+                                    <div className="form-group col-lg-5">
+                                        <label for="inputPatientName">Password</label>
+                                        <input type="password" onChange={(event) => setPassword(event.target.value)} className="form-control" id="inputPatientName" placeholder="XXXXXXXXX" />
+                                    </div>
+                                </div>
+                                <div className="btn-box">
+                                    <button type="submit" onClick={()=>login(event)} className="btn ">Login</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
