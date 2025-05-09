c38185f7cf4f99e11ea84a289e4d8d12d6433d01
Shivam Lowanshi <shivamlowanshi@gmail.com>
2025-05-09 20:50:51 +0530

    Node day-1

---
+++ Node/Basics/project-1/index.js
@@ -0,0 +1,35 @@
+
+const fs = require('fs');
+
+console.log("Project started");
+// fs.readFile('dummy.txt', 'utf-8', (err, data)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log(data)
+//     }
+// });
+
+const data = fs.readFileSync('dummy.txt', 'utf-8');
+console.log(data)
+
+fs.writeFile('dummy.txt', 'It is a new line', (err)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log('File written');
+    }
+});
+
+function hello(){
+    console.log('This a JS function');
+}
+
+hello();
+
+
+//Append, delete, create or delete a folder (Directory)
+
+
